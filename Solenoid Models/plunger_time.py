812f19b3478c392b943f8d958c41a9045e16f99c
Joe Spall <jspall0327@gmail.com>
2023-03-13 18:09:00 -0400

    Add average force, travel distance

--- Solenoid Models/plunger_time.py
+++ Solenoid Models/plunger_time.py
@@ -15,9 +15,9 @@ v_f = 0 # m/s
 
 d_total = 0 # m
 a_inst = 0 # m/s^2
-m = 0.0538641 # Kg
+m = 0.0538641 # kg
 delta_t = 0.1 # s
-travel_dist = 0.0124999961582 # m TODO confirm travel distance
+travel_dist = 0.015 # m
 for t_whole in range(0,20):
     femm.mi_analyze()
     femm.mi_loadsolution()
@@ -42,6 +42,8 @@ for t_whole in range(0,20):
     femm.mi_movetranslate(0, delta_d*METER_TO_IN) # Inches
 
 femm.closefemm()
+
+print("Avg. Force:" + repr(sum(f)/len(f)))
 plt.subplot(1,2,1)
 plt.plot(t,z)
 plt.xlabel('Time, s')
