43a5b8d9dc0ef5acefcb718dfaf131a995448f31
Matthew Barulic <matthew.barulic@gmail.com>
2023-03-13 22:08:06 -0400

    Fixes to solenoid physics sim

--- Solenoid Models/plunger_time.py
+++ Solenoid Models/plunger_time.py
@@ -1,3 +1,4 @@
+import math
 import femm
 import matplotlib.pyplot as plt
 
@@ -15,10 +16,10 @@ v_f = 0 # m/s
 
 d_total = 0 # m
 a_inst = 0 # m/s^2
-m = 0.0538641 # kg
-delta_t = 0.1 # s
+m = 0.061 # kg
+delta_t = 0.0001 # s
 travel_dist = 0.015 # m
-for t_whole in range(0,20):
+for t_whole in range(0,60):
     femm.mi_analyze()
     femm.mi_loadsolution()
     femm.mo_groupselectblock(1)
@@ -30,9 +31,9 @@ for t_whole in range(0,20):
     v_f = v_i + a_inst*delta_t
     delta_d = (v_i + v_f)/2 * delta_t
     d_total = d_total + delta_d
-    t.append(t,t_whole*delta_t)
-    z.append(z,d_total)
-    f.append(f,f_inst)
+    t.append(t_whole*delta_t)
+    z.append(d_total)
+    f.append(f_inst)
     if (d_total >= travel_dist):
         # Gone to end of stop
         break
@@ -42,6 +43,17 @@ for t_whole in range(0,20):
     femm.mi_movetranslate(0, delta_d*METER_TO_IN) # Inches
 
 femm.closefemm()
+
+# plunger weight: 55g
+# boot weight: 6g
+# total kicker weight: 61g
+
+
+# E = m * v^2
+# m_ball * v_ball^2 = m_plunger * v_plunger^2
+# v_ball = sqrt((m_plunger * v_plunger^2) / m_ball)
+v_ball = 0.8*math.sqrt((m * v_f**2) / 0.0459)
+print(f"Expected ball velocity: {v_ball} m/s")
 
 print("Avg. Force:" + repr(sum(f)/len(f)))
 plt.subplot(1,2,1)
